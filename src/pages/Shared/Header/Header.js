84a93935b4ec6da7670e19a44769f44a4d02627b
tasmia-rahman-maliha <maliha1288@gmail.com>
2022-10-25 21:20:54 +0600

    header done

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -6,6 +7,7 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
+    const [toggleTheme, setToggleTheme] = useState(false);
     return (
         <Navbar collapseOnSelect expand="lg" bg="white" variant="light">
             <Container>
@@ -15,12 +17,12 @@ const Header = () => {
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Link to='/home'>Home</Link>
-                        <Link to='/courses'>Courses</Link>
-                        <Link to='/blog'>Blog</Link>
-                        <Link to='/faq'>FAQ</Link>
-
+                    <Nav className="ms-auto">
+                        <Link to='/home' className='nav-link'>Home</Link>
+                        <Link to='/courses' className='nav-link'>Courses</Link>
+                        <Link to='/blog' className='nav-link'>Blog</Link>
+                        <Link to='/faq' className='nav-link'>FAQ</Link>
+                        <button className={`theme-btn ${toggleTheme ? 'light' : 'dark'}`} onClick={() => setToggleTheme(!toggleTheme)}>{toggleTheme ? 'Light' : 'Dark'}</button>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
