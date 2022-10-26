3d08d5363a797a1c1c19b8e77e231fcd75896981
tasmia-rahman-maliha <maliha1288@gmail.com>
2022-10-26 22:38:03 +0600

    login and logout done

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,18 +1,29 @@
 import React, { createContext } from 'react';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
 import { app } from '../../firebase/firebase.config';
+import { useState, useEffect } from 'react';
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-    const user = { displayName: 'Maliha' };
 
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
-    const authInfo = { user, createUser };
+    const login = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logout = () => {
+        return signOut(auth);
+    }
+
+
+
+
+    const authInfo = { createUser, login, logout };
 
     return (
         <AuthContext.Provider value={authInfo}>
--- src/pages/Login/Login/Login.js
+++ src/pages/Login/Login/Login.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const Login = () => {
+    const { login } = useContext(AuthContext);
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        login(email, password)
+            .then(result => {
+                const user = result.user;
+                form.reset();
+                navigate('/');
+            })
+            .catch(error => console.error(error))
+    }
+
     return (
         <div className='form-div'>
             <h3 className='text-center mb-5'>Login</h3>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" name="email" placeholder="Email" required />
